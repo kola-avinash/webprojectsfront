99bb704456c0c1a394cf66ff031ad8fd00948e64
Avinash Kola <avinashkola2001@gmail.com>
2024-12-02 21:15:42 -0500

    accessed data from frontend

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>  
+    <ProjectBox :projects="projects" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProjectBox from './components/ProjectBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProjectBox
+  },
+  data() {
+    return {
+      projects:[]
+    }
+  },
+  methods: {
+   async getProjects() {
+    const response = await fetch('https://webprojectsback.onrender.com/api')
+    const data = await response.json()
+    return data
+   }
+  },
+  async created() {
+    this.projects = await this.getProjects()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/ProjectBox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div> 
+      {{ projects }}
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ProjectBox',
+    props: {
+      projects: Array
+    }
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
+
+  
