b49024265a1754ff6e290cccf9fb9fb44a0e9ecb
Avinash Kola <avinashkola2001@gmail.com>
2024-12-02 21:42:11 -0500

    added single project component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>  
     <ProjectBox :projects="projects" />
+    <SingleBox :projects="projects" />
   </div>
 </template>
 
 <script>
 import ProjectBox from './components/ProjectBox.vue'
+import SingleBox from './components/SingleBox.vue'
 
 export default {
   name: 'App',
   components: {
-    ProjectBox
+    ProjectBox,
+    SingleBox
   },
   data() {
     return {
@@ -20,12 +23,15 @@ export default {
   methods: {
    async getProjects() {
     const response = await fetch('https://webprojectsback.onrender.com/api')
+    // const response = await fetch('http://localhost:5959/api')
     const data = await response.json()
     return data
    }
   },
   async created() {
     this.projects = await this.getProjects()
+    console.log(this.projects)
+    
   }
 }
 </script>
---
+++ src/components/SingleBox.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div v-for="project in projects" :key="project.id">
+            <h1>{{ project.id }}</h1>
+            <h1>{{ project.title }}</h1>
+            <h1>{{ project.description }}</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'SingleBox',
+    props: {
+        projects: Array
+    }
+}
+</script>
+<style scoped>
+
+</style>
